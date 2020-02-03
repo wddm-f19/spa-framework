0d2321967f5aca6255ff3a0ec05eef677a39ebbf
Rocco Panacci <n01340339@nj-00118-001.humber.org>
2020-02-03 13:00:47 -0500

    Fixed broken links

--- js/index.js
+++ js/index.js
@@ -2,28 +2,32 @@ const $home =    document.getElementById('home')
 const $about =   document.getElementById('about')
 const $contact = document.getElementById('contact')
 const $main =    document.querySelector('.main')
-
 const page = {
 	home: '<h2>Home</h2>',
 	about: '<h2>About</h2>',
 	contact: '<h2>Contact</h2>'
 }
 
+const gotoPage = (name) => {
+	if (!page[name])
+		name = 'home'  // Actually, do a "404"
+	$main.innerHTML = page[name]
+}
 
 $home.addEventListener('click', event => {
-	$main.innerHTML = page['home']
+	gotoPage('home')
 })
 
 $about.addEventListener('click', event => {
-	$main.innerHTML = page['about']
+	gotoPage('about')
 })
 
 $contact.addEventListener('click', event => {
-	$main.innerHTML = page['contact']
+	gotoPage('contact')
 })
 
 
 window.addEventListener('load', event => {
-	console.log(window.location.hash)
+	gotoPage(window.location.hash.slice(1))
 })
 

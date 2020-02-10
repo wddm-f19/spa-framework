32ee268d673addabbc2a31c280bbd049f1740458
Rocco Panacci <n01340339@nj-00118-001.humber.org>
2020-02-10 10:08:54 -0500

    Routing semi-working

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,7 @@ import ContactPage from '/js/pages/contact.js'
 const $routes = document.querySelectorAll('.router a')
 const $main =   document.querySelector('.main')
 
+
 const page = {
 	home: new HomePage(),
 	about: new AboutPage(),
@@ -14,16 +15,28 @@ const page = {
 const gotoPage = (name) => {
 	if (!page[name])
 		name = 'home'  // Actually, do a "404"
-	$main.innerHTML = page[name].getHTML();
+
+	window.history.pushState({}, '', `/${name}`)
+	$main.innerHTML = page[name].getHTML()
 }
 
 window.addEventListener('load', event => {
 	gotoPage(window.location.pathname.slice(1).split('/')[0])
 
+	const rand = Math.random()*10
+	console.log(rand);
+
 	$routes.forEach($link => {
 		$link.addEventListener('click', event => {
+			event.preventDefault()
 			gotoPage($link.getAttribute('href').slice(1).split('/')[0])
 		})
 	})
 
+})
+
+window.addEventListener('popstate', event => {
+	
+	console.log(event);
+	
 })
